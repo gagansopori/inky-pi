5160b60fcc8ce1eb45c1d67381bc67cf9cb49d49
Gagan Sopori <9592538+gagansopori@users.noreply.github.com>
2023-02-04 19:05:59 -0600

    resizing logic

--- src/service/__init__.py
+++
@@ -1,6 +0,0 @@
-from inky.auto import auto
-
-class InitializeInkyDisplay:
-    def __init__(self):
-        self.inky_display = auto(verbose=True)
-        self.inky_display.set_border(self.inky_display.WHITE)
