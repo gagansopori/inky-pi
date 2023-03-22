954bea04d96091a9acb4bc7d4fdf2773b015cb9a
Gagan Sopori <9592538+gagansopori@users.noreply.github.com>
2023-03-22 11:16:23 -0500

    Fixing Css

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from sources.webservice import UserAuthentication
 
 
 def create_app(config=None):
     app = Flask(__name__)
+    app.register_blueprint(UserAuthentication.bp)
 
     @app.route("/")
     def landing_page():
-        return render_template('index.html')
+        # TODO: If user is logged in, redirect to home page, else redirect to login page
+        return render_template('login.html')
+
+    # @app.route("/login/")
+    # def login():
+    #     return render_template('login.html')
+
+
 
     return app
