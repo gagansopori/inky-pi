664d387539e856c54538a4a4170253db6eef4b68
Gagan Sopori <gagan.sopori@hotmail.com>
2026-02-19 00:38:40 -0600

    some changes to installer & db manager

--- core/widgets/calendar.py
+++ core/widgets/calendar.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 from .base import Widget, WidgetState
 
-class ClockMini(WidgetState):
+class CalendarMini(WidgetState):
     def render(self, draw, x, y, w, h, data):
         draw.text((x + 40, y + 50), data['time'], fill=1)
 
-class ClockMaxi(WidgetState):
+class CalendarMaxi(WidgetState):
     def render(self, draw, x, y, w, h, data):
         draw.text((80, 110), data['time'], fill=1)
 
-class ClockWidget(Widget):
+class CalendarWidget(Widget):
     def fetch_data(self):
         self.data = {"time": datetime.now().strftime("%H:%M")}
